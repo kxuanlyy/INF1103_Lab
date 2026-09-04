1e95188fb4ee2df90879ee2ee50207ea4020dad5
Your name <your_name@example.com>
2026-09-04 15:38:53 +0800

    Fourth Commit

--- hello.py
+++ hello.py
@@ -21,3 +21,14 @@ print("Day 2:", followers)
 
 followers -= 10
 print("Day 3:", followers)
+
+username = input("Enter username:")
+age = input("Enter age:")
+category = input("Enter content category:")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
