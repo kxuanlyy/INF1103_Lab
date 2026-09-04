2a7638b8883eabbc19d4742d8e3ad49c2e09f1ea
Your name <your_name@example.com>
2026-09-04 15:32:16 +0800

    Second Commit

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("===================")
 print("Welcome here")
 print("My first post here!")
 print("===================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
